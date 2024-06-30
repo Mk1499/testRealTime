a267b3413cc1fb8d729644827e8c5c7d7fb104af
W855HAMO <w588hamo@saibnt151.com.sa>
2024-06-30 15:54:42 +0300

    first commit

---
+++ components/DribbleShots/DribbleShots.tsx
@@ -0,0 +1,11 @@
+import {View, Text} from 'react-native';
+import React from 'react';
+import Animated, {useAnimatedScrollHandler} from 'react-native-reanimated';
+
+export default function DribbleShots() {
+  return (
+    <View>
+      <Text>DribbleShots</Text>
+    </View>
+  );
+}
